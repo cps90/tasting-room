ec5d11eb66e215b7d92a6d68d9725b0a35d99679
cps90 <cpswedenburg@gmail.com>
2018-08-06 15:17:05 -0600

    set up environment to deploy

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const path = require("path")
+const port = process.env.PORT || 5800
+const secret = process.env.SECRET || "some secret passphrase here for local development"
+const dotenv = require('dotenv').config()  
+
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(dotenv())
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/taskmanager', (err => {
+    if (err) throw err
+    console.log('Connected to database')
+}))
+
+app.use('/wine', require('./routes/wine'))
+app.use('/food', require('./routes/food'))
+
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port 5800')
+})
+
