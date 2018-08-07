8eee3287b5fe47f7d31144ffb9980bf408cd82e9
cps90 <cpswedenburg@gmail.com>
2018-08-06 21:18:07 -0600

    fixed node version

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const morgan = require('morgan')
 const path = require("path")
 const port = process.env.PORT || 5800
 const secret = process.env.SECRET || "some secret passphrase here for local development"
-const dotenv = require('dotenv').config()  
+require('dotenv').config()  
 
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, "client", "build")))
-app.use(dotenv())
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/taskmanager', (err => {
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/taskmanager', (err) => {
     if (err) throw err
     console.log('Connected to database')
-}))
+})
 
 app.use('/wine', require('./routes/wine'))
 app.use('/food', require('./routes/food'))
