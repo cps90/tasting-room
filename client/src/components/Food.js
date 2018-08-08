62c3602dbdf21cdc6b7eea7ec118fae5edbf421d
cps90 <cpswedenburg@gmail.com>
2018-08-08 11:57:10 -0600

    make images clickable

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -3,9 +3,12 @@ import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router-dom'
 
 function Food(props) {
+    handleClick = () => {
+        <Link to={`/food/${props.endpoint}`}> {props.name} </Link>
+    }
     return (
         <div className="foods">
-                <img className="foodImage" src={props.image}/>
+                <img className="foodImage" src={props.image} onClick={this.handleClick}/>
                 <Link className="foodName" to={`/food/${props.endpoint}`}> {props.name} </Link>
         </div>
     );
